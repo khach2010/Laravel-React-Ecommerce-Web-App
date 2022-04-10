ce0977029bc751802fd35a7309e5aa34e04df7e6
tu le <khach2010@gmail.com>
2022-04-10 06:24:58 +0200

    Feature Products Component

--- User Web Application frontend/ecom/src/pages/HomePage.jsx
+++ User Web Application frontend/ecom/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react'
+import FeatureProducts from '../components/home/FeatureProducts'
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
         <h1>Home page here</h1>
+        <FeatureProducts />
       </Fragment>
     )
   }
