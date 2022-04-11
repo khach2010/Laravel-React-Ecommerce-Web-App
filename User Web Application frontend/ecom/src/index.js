6ab215f1cce03745507f809125d8d6f6ef2d3492
tu le <khach2010@gmail.com>
2022-04-11 09:38:22 +0200

    add fontawesome package - newArrival component

--- User Web Application frontend/ecom/src/index.js
+++ User Web Application frontend/ecom/src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../src/assets/css/custom.css'
-import '../src/assets/css/fontawesome.css'
 import '../src/assets/css/animate.min.css'
 
 ReactDOM.render(
--- User Web Application frontend/ecom/src/pages/HomePage.jsx
+++ User Web Application frontend/ecom/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import Categories from '../components/home/Categories'
 import Collections from '../components/home/Collections'
 import FeatureProducts from '../components/home/FeatureProducts'
+import NewArrival from '../components/home/NewArrival'
 
 class HomePage extends Component {
   render() {
@@ -10,6 +11,7 @@ class HomePage extends Component {
         <h1>Home page here</h1>
         <FeatureProducts />
         <Collections />
+        <NewArrival />
         <Categories />
       </Fragment>
     )
