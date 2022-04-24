1fd406b5f37df835b3dd0a7af5b8d27cfcc1c38c
tu le <khach2010@gmail.com>
2022-04-24 07:33:32 +0200

    Contact Component and Contact page

--- User Web Application frontend/ecom/src/pages/ContactPage.jsx
+++ User Web Application frontend/ecom/src/pages/ContactPage.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import FooterDesktop from '../components/common/FooterDesktop'
+import FooterMobile from '../components/common/FooterMobile'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
+import Contact from '../components/common/Contact'
 
 class ContactPage extends Component {
   render() {
     return (
-      <div>ContactPage</div>
+      <>
+      <div className="Desktop">
+        <NavMenuDesktop />
+      </div>
+
+      <div className="Mobile">
+        <NavMenuMobile />  
+      </div>      
+    
+          <Contact />
+
+      <div className="Desktop">
+        <FooterDesktop/>
+      </div>
+
+      <div className="Mobile">
+        <FooterMobile/>
+      </div>
+      
+    </>
     )
   }
 }
