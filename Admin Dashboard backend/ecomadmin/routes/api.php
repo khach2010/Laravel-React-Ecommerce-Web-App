5c25e04d82e93ab79c3087dfcc03a161d365f935
tu le <khach2010@gmail.com>
2022-05-10 06:38:43 +0200

    Create contact rest api

--- Admin Dashboard backend/ecomadmin/routes/api.php
+++ Admin Dashboard backend/ecomadmin/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -11,3 +12,4 @@ use App\Http\Controllers\Admin\VisitorController;
 
 
 Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
+Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
