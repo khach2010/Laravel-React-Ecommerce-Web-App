bb0cae3ad3cc3816d0579fc70996133aa1180fb4
tu le <khach2010@gmail.com>
2022-05-23 11:34:06 +0200

    Select Category Subcategory and make JSON Array

--- Admin Dashboard backend/ecomadmin/routes/api.php
+++ Admin Dashboard backend/ecomadmin/routes/api.php
@@ -5,13 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-
+// Get Visitor
 Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
+// Contact Page Route
 Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
+// Site Info Route
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
+// ALl Category Route
+Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
