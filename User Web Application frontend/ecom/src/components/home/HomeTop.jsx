dbe0e45c9298391acd62fb05d0ecf6e195e155f5
tu le <khach2010@gmail.com>
2022-05-24 06:54:11 +0200

    Consume Category Subcategory API part1

--- User Web Application frontend/ecom/src/components/home/HomeTop.jsx
+++ User Web Application frontend/ecom/src/components/home/HomeTop.jsx
@@ -1,16 +1,33 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Container,Row,Col,Card} from 'react-bootstrap'
 import MegaMenuDesktop from './MegaMenuDesktop'
 import HomeSlider from './HomeSlider'
+import AppURL from '../../api/AppURL'
+import axios from 'axios'
 
-class HomeTop extends Component {
-  render() {
+function HomeTop() {
+  const [menuData,  setMenuData] = useState([])
+
+  async function getMenuData() {
+    try {
+      const response = await axios.get(AppURL.AllCategoryDetails)
+      setMenuData(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMenuData()
+  }, []);
+
+
     return (
       <>
         <Container className="p-0 m-0 overflow-hidden" fluid={true}>
               <Row>
                   <Col lg={3} md={3} sm={12}>
-                  <MegaMenuDesktop />
+                  <MegaMenuDesktop data={menuData} />
                   </Col>
 
                   <Col lg={9} md={9} sm={12}>
@@ -20,7 +37,7 @@ class HomeTop extends Component {
         </Container>
       </>
     )
-  }
+  
 }
 
 export default HomeTop
--- User Web Application frontend/ecom/src/components/home/MegaMenuDesktop.jsx
+++ User Web Application frontend/ecom/src/components/home/MegaMenuDesktop.jsx
@@ -1,203 +1,52 @@
 import React, { Component } from 'react'
 
-class MegaMenuDesktop extends Component {
-
- componentDidMount() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if(panel.style.maxHeight){
-        panel.style.maxHeight = null;
-      }else{
-            panel.style.maxHeight= panel.scrollHeight+ "px"
-      }
-    });
-  }
- }
-
-
-  render() {
-       return (
-           <div className="accordionMenuDiv">
-                <div className="accordionMenuDivInside">
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button className="accordion">
-                      <img className="accordionMenuIcon" src="https://t3.ftcdn.net/jpg/02/99/52/12/240_F_299521250_KYMpMWGPMwgSODiVhssX8Ma6woPzdkRn.jpg" />&nbsp; Men's Clothing 
-                    </button>
-                    <div className="panel">
-                      <ul>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-                          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-                      </ul>
-                    </div>
-                  </div>
-
-
+function MegaMenuDesktop({data}) {
+  const MyView = data.map((cat, i) => {
+    const {category_name, category_image, subcategory_name} = cat
+    
+    return <div key={i.toString} >
+                <button className="accordion">
+                  <img className="accordionMenuIcon" src={category_image} />&nbsp; {category_name}
+                </button>
+                <div className="panel">
+                  <ul>
+                    {subcategory_name.map((sub, ix) => {
+                      return <li key={ix}><a href="#" className="accordionItem" > {sub.subcategory_name} </a></li>
+                    })}
+                  </ul>
                 </div>
-           </div>
-       )
+              </div> 
+  })
+
+  function MegeMenu(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        }else{
+              panel.style.maxHeight= panel.scrollHeight+ "px"
+        }
+      });
+    }
   }
+
+  MegeMenu()
+ 
+  return (
+      <div className="accordionMenuDiv">
+          <div className="accordionMenuDivInside">
+
+            {MyView}
+
+          </div>
+      </div>
+  )
+  
 }
 
 export default MegaMenuDesktop
