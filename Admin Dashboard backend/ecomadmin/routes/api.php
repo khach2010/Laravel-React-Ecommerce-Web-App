4ffe5f896d5a2ef3838915b5681974af2b2f3212
tu le <khach2010@gmail.com>
2022-05-28 06:56:44 +0200

    Create product list API part1

--- Admin Dashboard backend/ecomadmin/routes/api.php
+++ Admin Dashboard backend/ecomadmin/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductListController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -20,3 +21,5 @@ Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
 // ALl Category Route
 Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
+// ProductList Route
+Route::get('/productlistbyremark/{remark}', [ProductListController::class, 'ProductListByRemark']);
