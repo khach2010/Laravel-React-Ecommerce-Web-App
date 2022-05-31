c26e10d4697efb6fe9537f45c397f1e1f42716f7
tu le <khach2010@gmail.com>
2022-05-31 17:02:46 +0200

    Consume product list API part3

--- User Web Application frontend/ecom/src/components/home/NewArrival.jsx
+++ User Web Application frontend/ecom/src/components/home/NewArrival.jsx
@@ -1,168 +1,129 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Container,Row, Card} from 'react-bootstrap'
 import Slider from "react-slick";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import AppURL from '../../api/AppURL'
+import axios from 'axios'
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-class NewArrival extends Component {
-  constructor(props) {
-    super(props);
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
+function NewArrival() {
+  const [newArrival, setNewArrival] = useState([])
+  const [slider , setSlider] = useState()
+
+  const myView = newArrival.map((product, i) => {
+    const {title, price, image, special_price} =  product
+    if(special_price === 'na') {
+      return  <div key={i}>
+                <Card className="image-box card">
+                  <img className="center" src={image} />   
+                  <Card.Body> 
+                  <p className="product-name-on-card">{title}</p>
+                  <p className="product-price-on-card">Price : ${price}</p>
+
+                  </Card.Body>
+                </Card>
+              </div>
+    } else {
+      return  <div key={i}>
+                <Card className="image-box card">
+                  <img className="center" src={image} />   
+                  <Card.Body> 
+                  <p className="product-name-on-card">{title}</p>
+                  <p className="product-price-on-card">Price : <strike>${price}</strike> - ${special_price}</p>
+
+                  </Card.Body>
+                </Card>
+              </div>
+    }
+  })
+
+  async function getNewArrivalProducts() {
+    try {
+      const response = await axios.get(AppURL.ProductListByRemark("New"))
+      setNewArrival(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  next() {
-    this.slider.slickNext();
+
+  useEffect(() => {
+    getNewArrivalProducts()
+  }, []);
+
+
+  function next() {
+    slider.slickNext();
   }
-  previous() {
-    this.slider.slickPrev();
+  function previous() {
+    slider.slickPrev();
   }
-    render() {
-      var settings = {
-           dots: false,
-           infinite: true,
-           speed: 500,
-           autoplay: true,
-           autoplaySpeed:3000,
-           slidesToShow: 4,
-           slidesToScroll: 1,
-           initialSlide: 0,
-           arrows: false,
-           responsive: [
-             {
-               breakpoint: 1024,
-               settings: {
-                 slidesToShow: 4,
-                 slidesToScroll: 1,
-                 infinite: true,
-                 dots: true
-               }
-             },
-             {
-               breakpoint: 600,
-               settings: {
-                 slidesToShow: 2,
-                 slidesToScroll: 1,
-                 initialSlide: 2
-               }
-             },
-             {
-               breakpoint: 480,
-               settings: {
-                 slidesToShow: 1,
-                 slidesToScroll: 1
-               }
-             }
-           ]
-         };
+  
+  var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed:3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
 
-    return (
-      <>
-          <Container className="text-center" fluid={true}>
-          
-            <div className="section-title text-center mb-55">
-              <h2>NEW ARRIVAL &nbsp;
-                <a className="btn btn-sm ml-2 site-btn" onClick={this.previous} > <FontAwesomeIcon icon={faArrowLeft} /></a> 
-                &nbsp;
-                <a className="btn btn-sm ml-2 site-btn" onClick={this.next} > <FontAwesomeIcon icon={faArrowRight} /></a>
-              </h2>
-             
-              <p>Some Of Our Exclusive Collection, You May Like</p>
-            </div>
+  return (
+    <>
+        <Container className="text-center" fluid={true}>
+        
+          <div className="section-title text-center mb-55">
+            <h2>NEW ARRIVAL &nbsp;
+              <a className="btn btn-sm ml-2 site-btn" onClick={previous} > <FontAwesomeIcon icon={faArrowLeft} /></a> 
+              &nbsp;
+              <a className="btn btn-sm ml-2 site-btn" onClick={next} > <FontAwesomeIcon icon={faArrowRight} /></a>
+            </h2>
+            
+            <p>Some Of Our Exclusive Collection, You May Like</p>
+          </div>
 
-            <Row>
+          <Row>
 
-              <Slider ref={c => (this.slider = c)}  {...settings}>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/k7z3afk0/watch/t/c/x/lcs-8188-lois-caron-original-imafq3k9ztzdkyhe.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
+            <Slider ref={c => (setSlider(c))}  {...settings}>
+                {myView}
+            </Slider>
 
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jeka07k0/watch/4/p/y/38024pp25-fastrack-original-imaf37n5df3bn52n.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
+          </Row>
+        </Container>
 
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/kka1si80/watch/4/t/k/tw-02524-teenager-luxurious-fashion-silicone-black-colored-led-original-imafznht7bzfmj7d.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jw0zr0w0/watch/c/u/r/ls2811-limestone-original-imafgrxqf8qvecjd.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/kpodocw0/watch/p/l/t/anlg-428-blue-blu-analogue-original-imag3uxbhfkyhahf.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/k48rwcw0/watch/k/v/f/lcs-8190-lois-caron-original-imafn7fsyttnpybp.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-                <div>
-                  <Card className="image-box card">
-                    <img className="center" src="https://rukminim1.flixcart.com/image/800/960/ke353m80-0/watch/e/b/s/fresh-new-arrival-latest-men-watch-watches-men-ghadi-gents-boys-original-imafuupqgaanhtxu.jpeg?q=50" />   
-                    <Card.Body> 
-                    <p className="product-name-on-card">Realme C21 (Cross Black, 64 GB)</p>
-                    <p className="product-price-on-card">Price : $120</p>
-
-                    </Card.Body>
-                  </Card>
-                </div>
-              </Slider>
-
-            </Row>
-          </Container>
-
-      </>
-    )
-  }
+    </>
+  )
 }
 
 export default NewArrival
