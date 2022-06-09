85090d89110e899263d103d98775453d35ab4a7a
tu le <khach2010@gmail.com>
2022-06-09 06:18:52 +0200

    Consume product list API part 1 - ProductListByCategory

--- User Web Application frontend/ecom/src/components/ProductDetails/Category.jsx
+++ User Web Application frontend/ecom/src/components/ProductDetails/Category.jsx
@@ -4,26 +4,39 @@ import {Container,Row,Col,Card} from 'react-bootstrap'
 function Category({category, productCategoryData}) {
   const myView = productCategoryData.map((product, i) => {
     const {title, price, image, special_price} =  product
-    return  <div>
-              <Col key={i+title} className="p-0" xl={6} lg={6} md={6} sm={12} xs={12}>
-                
-                    <Card className="image-box card w-100">
-                        <img className="center w-75" src={image} alt={title}/>   
-                        <Card.Body> 
-                        <p className="product-name-on-card">{title}</p>
-                        <p className="product-price-on-card">Price : <strike>${price}</strike> - ${special_price}</p>
-
-                        </Card.Body>
-                      </Card> 
-                     
-                </Col>  
-            </div>
-   
+    if(special_price=="na") {
+      return   <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+                <Card className="image-box card w-100">
+                <img className="center w-75" src={image} />   
+                <Card.Body> 
+                <p className="product-name-on-card">{title}</p>
+                <p className="product-price-on-card">Price : ${price}</p>
+                </Card.Body>
+                </Card>          
+               </Col>
+    } else {
+      return   <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+                <Card className="image-box card w-100">
+                <img className="center w-75" src={image} />   
+                <Card.Body> 
+                <p className="product-name-on-card">{title}</p>
+                <p className="product-price-on-card">Price : <strike className="text-secondary">${price}</strike> ${special_price}</p>
+                </Card.Body>
+                </Card>          
+              </Col>
+    }
   })
+
+
   return (
-    <div>
-     {myView}
-    </div>
+    <Container className="text-center" fluid={true}>
+      <div className="section-title text-center mb-55">
+        <h2> {category }  </h2>
+      </div>
+      <Row> 
+              {myView}
+      </Row>
+    </Container>
   )
 }
 
