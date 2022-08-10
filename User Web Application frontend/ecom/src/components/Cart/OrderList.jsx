3ec87f071888c6cb0c8f4a60016197d548085f47
tu le <khach2010@gmail.com>
2022-08-10 08:00:45 +0200

    Product Order History part 3

--- User Web Application frontend/ecom/src/components/Cart/OrderList.jsx
+++ User Web Application frontend/ecom/src/components/Cart/OrderList.jsx
@@ -1,13 +1,21 @@
 import React, {useState, useEffect} from 'react'
-import {Navbar,Container, Row, Col,Button,Card} from 'react-bootstrap';
-import AppURL from '../../api/AppURL'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import {Container, Row, Col,Button,Card, Modal} from 'react-bootstrap';
+
 
 function OrderList({orderListHistory}) {
+  const [show, setShow] = useState(false);
+
+ 
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true);
+  
+  } 
+
   
   const myView = orderListHistory.map((order) => {
     const {product_name, quantity, size, color, unit_price, total_price, order_status, invoice_no} = order
+
     return <div>
             <Col key={invoice_no} md={6} lg={6} sm={6} xs={6}>
             <h5 className="product-name">{product_name}</h5>
@@ -16,7 +24,7 @@ function OrderList({orderListHistory}) {
             <h6>Price = {unit_price} x {quantity} = {total_price}$</h6>
             <h6>Status = {order_status} </h6>
             </Col>
-            <Button className="btn btn-danger">Post Review </Button>
+            <Button onClick={handleShow} className="btn btn-danger">Post Review </Button>
               <hr></hr>
          </div>
   })
@@ -25,20 +33,33 @@ function OrderList({orderListHistory}) {
     <>
         <h1>order list page </h1>
       <Container>
-    <div className="section-title text-center mb-55"><h2>Order History </h2>
-
-    </div>
+        <div className="section-title text-center mb-55">
+          <h2>Order History </h2>
+        </div>
 
         <Card >                
-    <Card.Body>
-    <Row>
-        {myView}
+          <Card.Body>
+          <Row>
+              {myView}
+          </Row>              
+          </Card.Body>               
+        </Card>
 
-    </Row>              
-    </Card.Body>               
-    </Card>
-
-
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Product review</Modal.Title>
+            
+          </Modal.Header>
+          <Modal.Body>Preview Details</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
       </Container>
     </>
