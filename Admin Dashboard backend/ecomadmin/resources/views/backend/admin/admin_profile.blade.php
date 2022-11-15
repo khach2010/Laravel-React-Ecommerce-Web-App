8d0f689c732084fcca082c6279619fd55f1c3ef5
tu le <khach2010@gmail.com>
2022-11-15 06:47:56 +0100

    Admin Profile & Image Update Part2

--- Admin Dashboard backend/ecomadmin/resources/views/backend/admin/admin_profile.blade.php
+++ Admin Dashboard backend/ecomadmin/resources/views/backend/admin/admin_profile.blade.php
@@ -36,10 +36,10 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex flex-column align-items-center text-center">
-                                    <img src="{{ asset('backend/assets/images/avatars/avatar-2.png')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+                                    <img src="{{ (!empty($adminData->profile_photo_path))?url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
                                     <div class="mt-3">
-                                        <h4>John Doe</h4>
-                                        <p class="text-secondary mb-1">Full Stack Developer</p>
+                                        <h4>{{$adminData->name}}</h4>
+                                        <p class="text-secondary mb-1">{{$adminData->email}}</p>
                                         <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
                                         <button class="btn btn-primary">Follow</button>
                                         <button class="btn btn-outline-primary">Message</button>
@@ -73,7 +73,7 @@
                                         <h6 class="mb-0">Full Name</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="John Doe">
+                                        <input type="text" class="form-control" value="{{$adminData->name}}">
                                     <div data-lastpass-icon-root="true" style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;"></div></div>
                                 </div>
                                 <div class="row mb-3">
@@ -81,33 +81,16 @@
                                         <h6 class="mb-0">Email</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="john@example.com">
+                                        <input type="text" class="form-control" value="{{$adminData->email}}">
                                     </div>
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Phone</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(239) 816-9029">
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Mobile</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(320) 380-4539">
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Address</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="Bay Area, San Francisco, CA">
-                                    </div>
-                                </div>
+
+                                <div class="mb-3">
+									<label for="formFile" class="form-label">Upload Profile Image</label>
+									<input class="form-control" type="file" id="formFile">
+								</div>
+
+
                                 <div class="row">
                                     <div class="col-sm-3"></div>
                                     <div class="col-sm-9 text-secondary">
